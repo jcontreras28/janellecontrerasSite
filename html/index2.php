3c77706b67e04ad075f8b67a9738383157b8ff4e
jcontreras28 <janellec@bendbroadband.com>
2015-08-28 10:50:02 -0700

    css

--- html/index2.php
+++ html/index2.php
@@ -47,6 +47,14 @@
                                         </div>
                                 </div>
                         </div>
+                        <div class="row sectionText">
+                                <p><strong>ANYTHING!</strong> First and formost I love to learn.  You give me a challenge and I will take it on with gusto. </p> 
+                                <p>For example, this site was a learning project.  I built an Amazon web server with Ubuntu Linux, Apache for a server, MySQL as the database and Php of course.  
+                                        The content is stored in a repo up on github which has a webhook to call a deploy script on my webserver whenever I push new content from my local machine.  
+                                        This script then goes and grabs the current site content.  Pretty basic setup that we use currently on most projects within my current company.
+                                        This project also uses sass for the css.  The sass code is compiled using codekit.
+                                </p>
+                        </div>
                 </div>
         </body>
 </html>
