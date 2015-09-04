75440e630609bb9e4b4f8f65a6ed590fa997dcf8
jcontreras28 <janellec@bendbroadband.com>
2015-09-04 14:50:56 -0700

    css

--- html/index.php
+++ html/index.php
@@ -30,21 +30,17 @@
                         </div><!-- mainNav -->
 
                         <div class="panelWrap selected" id="skills">
-                                <div class="sectionHeading">
-                                        <div class="makeTableDiv">
-                                                <div class="row tableContents">
-                                                        <div class="col-md-3 titlePic">
-                                                                <img src="./images/suncar.jpg">
-                                                        </div>
-                                                        <div class="col-md-9">
-                                                                <div class="sectionTitle">
-                                                                        What can I do?
-                                                                </div>
-                                                                <div class="sectionBar">
-                                                                </div>
-                                                        </div>
-                                                </div><!-- tableContents -->
-                                        </div><!-- makeTableDiv -->
+                                <div class="row sectionHeading">
+                                        <div class="col-md-3">
+                                                <img src="./images/suncar.jpg">
+                                        </div>
+                                        <div class="col-md-9">
+                                                <div class="sectionTitle">
+                                                        What can I do?
+                                                </div>
+                                                <div class="sectionBar">
+                                                </div>
+                                        </div>
                                 </div><!-- sectionHeading -->
                                 <div class="row sectionText">
                                         <p><strong>ANYTHING!</strong> First and formost I love to learn.  You give me a challenge and I will take it on with gusto. </p> 
