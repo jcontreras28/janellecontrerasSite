f30c745791e0f6831e39d8e068635fd62869af5a
jcontreras28 <janellec@bendbroadband.com>
2015-09-04 15:32:23 -0700

    css

--- html/index.php
+++ html/index.php
@@ -27,15 +27,15 @@
                                         <li class="tabItem" id="lidream">Dream</li>
                                         <li class="tabItem" id="liabout">About</li>
                                 </ul>
-                                <div><i class="fa fa-bars fa-3x menuSandwhich"></i></div>
                         </div><!-- mainNav -->
                         <div class="mobileNav">
-                                <ul>
-                                        <li class="tabItem menuItemSelected" id="liskills">Skills</li>
-                                        <li class="tabItem" id="liexperience">Work experience</li>
-                                        <li class="tabItem" id="lidream">Dream</li>
-                                        <li class="tabItem" id="liabout">About</li>
-                                </ul>
+                                <div><i class="fa fa-bars fa-3x menuSandwhich"></i></div>
+                                <div class="mobileItems" style="display:none">
+                                        <div class="tabItem menuItemSelected" id="liskills">Skills</div>
+                                        <div class="tabItem" id="liexperience">Work experience</div>
+                                        <div class="tabItem" id="lidream">Dream</div>
+                                        <div class="tabItem" id="liabout">About</div>
+                                </div>
                         </div><!-- mainNav -->
 
                         <div class="panelWrap selected" id="skills">
