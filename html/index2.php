7754cd447ef31291dca03d97723fe855b543a388
jcontreras28 <janellec@bendbroadband.com>
2015-09-15 16:32:00 -0700

    new files

---
+++ html/index2.php
@@ -0,0 +1,29 @@
+<?php
+?>
+
+<!doctype html>
+
+<html>
+        <head>
+                <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+                <meta name="viewport" content="width=device-width, intial-scale=1.0" />
+                <title>Janelle Contreras portfolio</title>
+                <meta name="description" content="Web developer programmer Janelle Contreras"/>
+                <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+                <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.js"></script> 
+                
+                <!-- Latest compiled and minified CSS -->
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+                <!-- Latest compiled and minified JavaScript -->
+                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+                <link rel="stylesheet" href="css/screen.css">
+        </head>
+        <body class="secondVersion">
+                <div class="topBackground">
+                        
+                </div>
+                <div class="bottomBackground">
+
+                </div>
+        </body>
+</html>
