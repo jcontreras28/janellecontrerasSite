688f673090ea8fa82fdf28dc86aa94866b66e922
jcontreras28 <janellec@bendbroadband.com>
2015-09-18 16:19:43 -0700

    new file

---
+++ html/contactForm.php
@@ -0,0 +1,15 @@
+<?php
+?>
+
+<!doctype html>
+
+<html>
+    <?php require 'header.php';?>        
+    <?php require 'navigation.php';?>
+                        
+    <div class="panelWrap selected" id="contactForm">xx
+    </div>
+
+
+    <?php require 'footer.php'; ?>
+</html>
