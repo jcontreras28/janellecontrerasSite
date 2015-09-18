ee2b063510ea8deb7532b066ac5b1e8814001436
jcontreras28 <janellec@bendbroadband.com>
2015-09-18 14:49:03 -0700

    css

--- html/index2.php
+++ html/index2.php
@@ -31,8 +31,131 @@
                                         <li class="tabItem" id="liabout">About</li>
                                 </ul>
                         </div>
+                        <div class="mobileNav">
+                                <div class="sandMenu"><i class="fa fa-bars fa-3x menuSandwhich"></i></div>
+                                <div class="mobileItems" style="display:none">
+                                        <div class="tabItem menuItemSelected" id="liskills">Skills</div>
+                                        <div class="tabItem" id="liexperience">Work experience</div>
+                                        <div class="tabItem" id="lidream">Dream</div>
+                                        <div class="tabItem" id="liabout">About</div>
+                                </div>
+                        </div><!-- mainNav -->
                 </div>
                 <div class="bottomBackground">
+                        <div class="panelWrap selected" id="skills">
+                                <div class="row sectionHeading">
+                                        <div class="col-md-3">
+                                                <img src="./images/suncar.jpg">
+                                        </div>
+                                        <div class="col-md-9">
+                                                <div class="sectionTitle">
+                                                        What can I do?
+                                                </div>
+                                                <div class="sectionBar">
+                                                </div>
+                                        </div>
+                                </div><!-- sectionHeading -->
+                                <div class="row sectionText">
+                                        <p><strong>ANYTHING!</strong> First and formost I love to learn.  You give me a challenge and I will take it on with gusto. </p> 
+                                        <p>For example, I built this site as an Amazon web server with Ubuntu Linux, Apache for a server, MySQL as the database and Php of course.  
+                                                I pointed it to my domain name using Route 53.
+                                                The content is stored in a repo up on github which has a webhook to call a deploy script on my webserver whenever I push new content from my local machine.  
+                                                This script then goes and grabs the current site content.  Pretty basic setup that we use currently on most projects within my current company.
+                                                This project also uses sass for the css.  The sass code is compiled using codekit.
+                                        </p>
+                                </div><!-- sectionText -->
+                                <div class="row logos">
+                                        <div class="col-md-4">
+                                                <img src="./images/logos/apache-logo-v2.png" style="width:80%"/>
+                                        </div>
+                                        <div class="col-md-4">
+                                                <img src="./images/logos/php_mysql_logo.jpg" style="width:80%"/>
+                                                
+                                        </div>
+                                        <div class="col-md-4">
+                                                <img src="./images/logos/ubuntu.png"/>
+                                        </div>
+                                </div>
+                                <div class="row logos">
+                                        <div class="col-md-3">
+                                                <img src="./images/logos/oscon2014_logo.png"/>
+                                        </div>
+                                        <div class="col-md-3">
+                                                <img src="./images/logos/codekit-logo-dark@2x.png"/>
+                                        </div>
+                                        <div class="col-md-3">
+                                                <img src="./images/logos/sass.png"/>
+                                        </div>
+                                        <div class="col-md-3">
+                                                <img src="./images/logos/github-logo.png"/>
+                                        </div>
+                                </div>
+                                <div class="windowwidth">click for window width</div>
+                        </div><!-- panelWrap -->
+                        <div class="panelWrap" id="experience">
+                                <div class="row sectionHeading">
+                                        <div class="col-md-3">
+                                                <img src="./images/fishin.jpg">
+                                        </div>
+                                        <div class="col-md-9">
+                                                <div class="sectionTitle">
+                                                        Is this work?
+                                                </div>
+                                                <div class="sectionBar">
+                                                </div>
+                                        </div>
+                                </div><!-- sectionHeading -->
+                                <div class="row sectionText">
+                                        <p><strong>Employment history</strong></p> 
+                                        <p>This will look different
+                                        </p>
+                                </div><!-- sectionText -->
+                                
+                                <div class="windowwidth">click for window width</div>
+                        </div><!-- panelWrap -->
+                        <div class="panelWrap" id="dream">
+                                <div class="row sectionHeading">
+                                        <div class="col-md-3">
+                                                <img src="./images/oscon.jpg">
+                                        </div>
+                                        <div class="col-md-9">
+                                                <div class="sectionTitle">
+                                                        What is my dream?
+                                                </div>
+                                                <div class="sectionBar">
+                                                </div>
+                                        </div>
+                                </div><!-- sectionHeading -->
+                                <div class="row sectionText">
+                                        <p><strong>Open source for all!</strong> Love going to OSCON</p> 
+                                        <p>Build amazing fun software!
+                                        </p>
+                                </div><!-- sectionText -->
+                                <div class="">
+                                        <img src="./images/logos/all-logos.png" style="width: 50%"/>
+                                </div>
+                                <div class="windowwidth">click for window width</div>
+                        </div><!-- panelWrap -->
+                        <div class="panelWrap" id="about">
+                                <div class="row sectionHeading">
+                                        <div class="col-md-3">
+                                                <img src="./images/softball.jpg">
+                                        </div>
+                                        <div class="col-md-9">
+                                                <div class="sectionTitle">
+                                                        Who am I?
+                                                </div>
+                                                <div class="sectionBar">
+                                                </div>
+                                        </div>
+                                </div><!-- sectionHeading -->
+                                <div class="row sectionText">
+                                        <p><strong>Amazing!</strong> First and formost I love to learn.  You give me a challenge and I will take it on with gusto. </p> 
+                                        <p>I love sports!
+                                        </p>
+                                </div><!-- sectionText -->
+                                <div class="windowwidth">click for window width</div>
+                        </div><!-- panelWrap -->
 
                 </div>
         </body>
