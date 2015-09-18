08365631fe2ee9cb9dfe4dff7cb02823c8b42093
jcontreras28 <janellec@bendbroadband.com>
2015-09-18 14:52:38 -0700

    css

--- html/index2.php
+++ html/index2.php
@@ -159,4 +159,33 @@
 
                 </div>
         </body>
+        <script type="text/javascript">
+                $('.tabItem').click(function() {
+                        var theId = $(this).attr('id');
+                        theId = theId.replace("li", "");
+
+                        // updating menu
+                        var oldTab = $('.menuItemSelected');
+                        oldTab.removeClass('menuItemSelected');
+
+                        $(this).addClass("menuItemSelected");
+
+                        // updating page
+                        var oldPanel = $('.selected')
+                        oldPanel.removeClass("selected")
+                        
+                        var newPanel = $('#' + theId);
+                        newPanel.addClass("selected");
+                });
+
+                $('.windowwidth').click(function(){
+                        alert("width= "+$(window).width());
+                });
+
+                $('.menuSandwhich').click(function() {
+                        $('.mobileItems').slideToggle(350);
+                });
+
+
+        </script>
 </html>
