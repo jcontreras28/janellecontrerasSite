f7c768dba0cd332a5ab71d5d67eb5cf2a8745744
jcontreras28 <janellec@bendbroadband.com>
2015-09-18 17:10:23 -0700

    css

--- html/header.php
+++ html/header.php
@@ -20,7 +20,7 @@
                                 <div class="col-sm-8 headerItem myName">
                                         <a href="janellecontreras.com">Janelle Contreras</a>
                                 </div>
-                                <div class="col-sm-4 headerItem">
+                                <div class="col-sm-4 headerItem myLinks">
                                         <i class="fa fa-github fa-4x"></i>
                                         <a href="http://www.linkedin.com/in/janellecontreras" target="_blank">
                                                 <i class="fa fa-linkedin-square fa-4x"></i>
