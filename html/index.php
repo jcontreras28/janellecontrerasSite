db44dc667bda55dae2e312aca73031b69826874f
jcontreras28 <janellec@bendbroadband.com>
2015-09-18 20:38:57 -0700

    css

--- html/index.php
+++ html/index.php
@@ -18,49 +18,9 @@
                         
             <?php require 'skillsPanel.php'; ?>
 
-            <div class="panelWrap" id="experience">
-                    <div class="row sectionHeading">
-                            <div class="col-md-3">
-                                    <img src="./images/fishin.jpg">
-                            </div>
-                            <div class="col-md-9">
-                                    <div class="sectionTitle">
-                                            Is this work?
-                                    </div>
-                                    <div class="sectionBar">
-                                    </div>
-                            </div>
-                    </div><!-- sectionHeading -->
-                    <div class="row sectionText">
-                            <p><strong>Employment history</strong></p> 
-                            <p>This will look different
-                            </p>
-                    </div><!-- sectionText -->
-                                    
-                    <div class="windowwidth">click for window width</div>
-            </div><!-- panelWrap -->
+            <?php require 'experience.php'; ?>
 
-            <div class="panelWrap" id="dream">
-                    <div class="row sectionHeading">
-                            <div class="col-md-3">
-                                    <img src="./images/oscon.jpg">
-                            </div>
-                            <div class="col-md-9">
-                                    <div class="sectionTitle">
-                                            What is my dream?
-                                    </div>
-                            </div>
-                    </div><!-- sectionHeading -->
-                    <div class="row sectionText">
-                            <p><strong>Open source for all!</strong> Love going to OSCON</p> 
-                            <p>Build amazing fun software!
-                            </p>
-                    </div><!-- sectionText -->
-                    <div class="">
-                            <img src="./images/logos/all-logos.png" style="width: 50%"/>
-                    </div>
-                    <div class="windowwidth">click for window width</div>
-            </div><!-- panelWrap -->
+            <?php require 'dreamPanel.php'; ?>
 
             <div class="panelWrap" id="about">
                     <div class="row sectionHeading">
