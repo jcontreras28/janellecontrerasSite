825e32a3cf4323f880ea75e5b1032f4a72b20617
jcontreras28 <janellec@bendbroadband.com>
2015-09-18 20:30:51 -0700

    css

--- html/navigation.php
+++ html/navigation.php
@@ -9,9 +9,11 @@
 </div><!-- mainNav -->
 <div class="mobileNav">
     <div class="sandMenu"><i class="fa fa-bars fa-3x menuSandwhich"></i></div>
+
     <div class="mobileItems" style="display:none">
-    <div class="tabItem menuItemSelected" id="liskills">Skills</div>
-    <div class="tabItem" id="liexperience">Resume</div>
-    <div class="tabItem" id="lidream">Dream</div>
-    <div class="tabItem" id="liabout">About</div>
+        <div class="tabItem menuItemSelected" id="liskills">Skills</div>
+        <div class="tabItem" id="liexperience">Resume</div>
+        <div class="tabItem" id="lidream">Dream</div>
+        <div class="tabItem" id="liabout">About</div>
+    </div><!-- mobileItems -->
 </div><!-- mobileNav -->
