4953e15be00523e2d48b7ca335a0a63fa264ed62
jcontreras28 <janellec@bendbroadband.com>
2015-09-18 21:13:13 -0700

    css

--- html/javascripts.php
+++ html/javascripts.php
@@ -8,7 +8,7 @@
                 // updating menu
                 var oldTab = $('.menuItemSelected');
                 oldTab.removeClass('menuItemSelected');
-                $('.'+jPageType).addClass("menuItemSelected");
+                $('#'+jPageType).addClass("menuItemSelected");
 
                 // updating page
                 var oldPanel = $('.selected')
