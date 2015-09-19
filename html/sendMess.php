47a6281097f15e39ad01d17aa5657dd2d3ad8b0a
jcontreras28 <janellec@bendbroadband.com>
2015-09-18 18:14:59 -0700

    contact form

---
+++ html/sendMess.php
@@ -0,0 +1,16 @@
+<?php
+
+require './phpmailer/PHPMailerAutoload.php';
+
+$to = 'janellec@bendbroadband.com';
+$subject = 'contact email from janellecontreras.com';
+$message = "Mesage from: ".$_POST("name")." - ".$_POST("email")."\n\r".$_POST("message");
+$headers = "From: janellecontreras.com \r\n" .
+			'Reply-To: janellec@bendbroadband.com' . "\r\n" .
+			'X-Mailer: PHP/'.phpversion();
+mail($to, $subject, $message, $headers);
+
+include 'thankYou.php';
+
+
+?>
