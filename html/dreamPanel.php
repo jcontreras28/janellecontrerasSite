69f7ced1e4f20862af0bce8570cf99245ac5a27c
jcontreras28 <janellec@bendbroadband.com>
2015-09-18 20:43:19 -0700

    new files

---
+++ html/dreamPanel.php
@@ -0,0 +1,20 @@
+<div class="panelWrap" id="dream">
+    <div class="row sectionHeading">
+        <div class="col-md-3">
+            <img src="./images/oscon.jpg">
+        </div>
+        <div class="col-md-9">
+            <div class="sectionTitle">
+                What is my dream?
+            </div>
+        </div>
+    </div><!-- sectionHeading -->
+    <div class="row sectionText">
+        <p><strong>Open source for all!</strong> Love going to OSCON</p> 
+        <p>Build amazing fun software!</p>
+    </div><!-- sectionText -->
+    <div class="">
+        <img src="./images/logos/all-logos.png" style="width: 50%"/>
+    </div>
+    <div class="windowwidth">click for window width</div>
+</div><!-- panelWrap -->
---
+++ html/experiencePanel.php
@@ -0,0 +1,19 @@
+<div class="panelWrap" id="experience">
+    <div class="row sectionHeading">
+        <div class="col-md-3">
+            <img src="./images/fishin.jpg">
+        </div>
+        <div class="col-md-9">
+            <div class="sectionTitle">
+                Is this work?
+            </div>
+
+        </div>
+    </div><!-- sectionHeading -->
+    <div class="row sectionText">
+        <p><strong>Employment history</strong></p> 
+        <p>This will look different</p>
+    </div><!-- sectionText -->
+                                    
+    <div class="windowwidth">click for window width</div>
+</div><!-- panelWrap -->
