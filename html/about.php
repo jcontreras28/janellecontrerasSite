0e18de5e6fc07d0cab2ae0963f3bdd2adae0064f
jcontreras28 <janellec@bendbroadband.com>
2015-10-18 11:29:14 -0700

    new files

---
+++ html/about.php
@@ -0,0 +1,19 @@
+<div class="panelWrap" id="about">
+    <div class="row sectionHeading">
+        <div class="col-md-3">
+            <img src="./images/softball.jpg">
+        </div>
+        <div class="col-md-9">
+            <div class="sectionTitle">
+                Who am I?
+            </div>
+            <div class="sectionBar">
+            </div>
+        </div>
+    </div><!-- sectionHeading -->
+    <div class="row sectionText">
+        <p><strong>Amazing!</strong> First and formost I love to learn.  You give me a challenge and I will take it on with gusto. </p> 
+        <p>I love sports!</p>
+    </div><!-- sectionText -->
+    <div class="windowwidth">click for window width</div>
+</div><!-- panelWrap -->
