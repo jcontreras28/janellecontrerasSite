7ff1ce1e12c694e4d974e674756f8b46ca35b5c3
jcontreras28 <janellec@bendbroadband.com>
2015-12-22 14:04:40 -0800

    pic

--- html/about.php
+++ html/about.php
@@ -14,7 +14,10 @@
         <div class="limitWidth">
             <div class="resSecTitle">I love sports :-)</div>    
             <p>First and formost I love to learn.  You give me a challenge and I will take it on with gusto. </p> 
-            <p>I love sports!</p>
+            <p>I love cats of course!  What self respecting developer/tech person doesn't?</p>
+            <div class="bigLogo">
+                <img src="./images/catpic.jpg" style="width: 50%;margin-left:25%"/>
+            </div>
         </div>
     </div><!-- sectionText -->
 </div><!-- panelWrap -->
