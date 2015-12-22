4df5adb9780cbe6e4fde5599e5a9dc1b4f88e9d3
jcontreras28 <janellec@bendbroadband.com>
2015-12-22 13:33:03 -0800

    content

--- html/dreamPanel.php
+++ html/dreamPanel.php
@@ -11,8 +11,8 @@
     </div><!-- sectionHeading -->
     <div class="row sectionText">
         <div class="limitWidth">
-            <p><strong>Open source for all!</strong> Love going to OSCON</p> 
-            <p>Build amazing fun software!</p>
+
+            <p>My true dream job integrates all three of my loves - mathematics, games/logic, and education.  My dream is to work on developing high end educational games aimed at high school level mathematics.  Games that are competive in quality to the ones kids love to play such as Call of Duty series, FIFA series, or others.</p>
     
             <div class="bigLogo">
                 <img src="./images/logos/all-logos.png" style="width: 50%;margin-left:25%"/>
