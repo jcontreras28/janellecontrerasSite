05a11e04ff7b19c7c3a279a0b4c0c5327c9b460f
jcontreras28 <janellec@bendbroadband.com>
2015-12-22 13:30:18 -0800

    css

--- html/dreamPanel.php
+++ html/dreamPanel.php
@@ -4,17 +4,20 @@
             <img class="meImage" src="./images/oscon.jpg">
         </div>
         <div class="col-md-9">
-            <div class="sectionTitle">
-                What is my dream?
+            <div class="resProfTitle">Open Source!</div> 
+            <div class="resText">Throug open source, we have the abillity for kids to learn to program. We haveethousands instead of a handfull develping the greatest most secure software possible.  We have the poor equally able to develop as large companies.  We have it all.
             </div>
         </div>
     </div><!-- sectionHeading -->
     <div class="row sectionText">
-        <p><strong>Open source for all!</strong> Love going to OSCON</p> 
-        <p>Build amazing fun software!</p>
+        <div class="limitWidth">
+            <p><strong>Open source for all!</strong> Love going to OSCON</p> 
+            <p>Build amazing fun software!</p>
+    
+            <div class="bigLogo">
+                <img src="./images/logos/all-logos.png" style="width: 50%;margin-left:25%"/>
+            </div>
+        </div>
     </div><!-- sectionText -->
-    <div class="">
-        <img src="./images/logos/all-logos.png" style="width: 50%"/>
-    </div>
-    <div class="windowwidth">click for window width</div>
+ 
 </div><!-- panelWrap -->
