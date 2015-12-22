206c304c437674492d4ba08f80ab56ebf8ce6148
jcontreras28 <janellec@bendbroadband.com>
2015-12-22 11:15:30 -0800

    css

--- html/experiencePanel.php
+++ html/experiencePanel.php
@@ -13,8 +13,38 @@
     </div><!-- sectionHeading -->
     <div class="row sectionText">
         <div class="limitWidth">
-            <p><strong>Employment history</strong></p> 
-            <p>This will look different</p>
+            <div class="resSecTitle">Profile</div> 
+            <div class="resText">I am a programmer that has worked on all aspects of web development as well as embeded systems.  
+                Basically, I can do it all.  My favorite types of projects involve my love of mathematics combined with the problem solving of writing code.  
+                In this line of work, I get to do puzzles all day long and get paid for it.
+            </div>
+            <div class="resSecTitle">Languages/Frameworks</div> 
+            <div class="resText">
+                <ul>
+                    <li>Excellent command over C, C++, Javascript/jQuery/ajax, Visual Basic, PhP, SQL, Cache(mumps), Swift</li>
+                    <li>Daily use of HTML5, CSS, Sass, Bootstrap, PhP, SQL</li>
+                    <li>Experience with Ruby, Perl, Go, Python, C#, Django</li>
+                </ul>
+            </div>
+            <div class="resSecTitle">Concepts used</div> 
+            <div class="resText">
+                <ul>
+                    <li>Proficient with artificial intelligence concepts such as predicate calculus, state space searches,heuristic searches, stochastic methods, and machine learning.</li>
+                    <li>Excellent skills in the mathematics behind computer graphics rendering, 3d modeling as well asany mathematical programming applications.</li>
+                    <li>Database work including query writing using SQL type syntax.</li>
+                    <li>Event/Interrupt driven programming with multithreading and multitasking.</li>
+                    <li>Using HTML5, CSS, and Javascript/jQuery to create responsive websites that could be turnedinto hybrid apps using Cordova/PhoneGap.</li>
+                    <li>Open source tools/development and versioning through Github.</li>
+                    <li>Created and used APIs using both data interchange formats like JSON and XML as well asfunctional in any language.</li>
+                    <li>Extremely skilled in problem solving and working in a team environment.</li>
+                </ul>
+            </div>
+            <div class="resSecTitle">Employment</div> 
+            <div class="resText">
+            </div>
+            <div class="resSecTitle">Education</div> 
+            <div class="resText">
+            </div>
         </div><!-- limitWidth -->
     </div><!-- sectionText -->
                                     
