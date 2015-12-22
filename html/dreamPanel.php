a27ed019c3f52cdc0e772dce0e43e6ffd7774818
jcontreras28 <janellec@bendbroadband.com>
2015-12-22 13:35:41 -0800

    css

--- html/dreamPanel.php
+++ html/dreamPanel.php
@@ -11,8 +11,9 @@
     </div><!-- sectionHeading -->
     <div class="row sectionText">
         <div class="limitWidth">
-
+            <div class="resSecTitle">Dream Job?</div> 
             <p>My true dream job integrates all three of my loves - mathematics, games/logic, and education.  My dream is to work on developing high end educational games aimed at high school level mathematics.  Games that are competive in quality to the ones kids love to play such as Call of Duty series, FIFA series, or others.</p>
+            <p>A dream job would also include some of the pieces that make work enjoyable.  First, a fun team to work with.  Through collaboration, we learn, focus, and achieve the best results.  My favorite thing is brainstorming with amazing people - developers, artists, and others to find the best solution to any problem.  Or often, we come up with the problem - the need that we must find a way to fill.</p>
     
             <div class="bigLogo">
                 <img src="./images/logos/all-logos.png" style="width: 50%;margin-left:25%"/>
