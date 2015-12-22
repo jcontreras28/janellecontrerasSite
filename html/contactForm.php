717b47070905c703e2ac229fa9ba103c20304b60
jcontreras28 <janellec@bendbroadband.com>
2015-12-22 14:10:49 -0800

    form

--- html/contactForm.php
+++ html/contactForm.php
@@ -2,19 +2,18 @@
     <div class="contactcontent">
         <div id="contact-form">
             <form action="javascript:void(0);" id="contactform" method="post" class="contactsubmit">
-                <div class="topRow">
-                    <div class="nameField">
-                        <label for="name">Name</label>
-                        <input type="text" name="name" id="name" minlength="2" size="22" tabindex="1" class="required" required/>
-                    </div><!-- nameField -->
-                    <br>
+                <div class="nameField">
+                    <label for="name">Name</label>
+                    <input type="text" name="name" id="name" minlength="2" size="22" tabindex="1" class="required" required/>
+                </div><!-- nameField -->
+                <br>
                     
-                </div><!-- topRow -->
                 <div class="emailField">
                         <label for="email">Email</label>
                         <input type="email" name="email" id="email" size="22" tabindex="1" class="required email" required/>
                     </div><!-- emailField -->
                 <div class="clearfix"></div>
+                
                 <div class="messageRow">
                     <label for="message">Message</label><br>
                     <textarea name="message" id="message" cols="70%" rows="8" tabindex="3" class="required" required></textarea>
