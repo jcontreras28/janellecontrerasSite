a817365237b8855014fc2cae90926550b8848dce
jcontreras28 <janellec@bendbroadband.com>
2015-12-22 14:05:46 -0800

    css

--- html/about.php
+++ html/about.php
@@ -12,9 +12,10 @@
     </div><!-- sectionHeading -->
     <div class="row sectionText">
         <div class="limitWidth">
-            <div class="resSecTitle">I love sports :-)</div>    
+            <div class="resSecTitle">I love sports</div>    
             <p>First and formost I love to learn.  You give me a challenge and I will take it on with gusto. </p> 
-            <p>I love cats of course!  What self respecting developer/tech person doesn't?</p>
+            <div class="resSecTitle">I love cats</div> 
+            <p>What self respecting developer/tech person doesn't love cats?</p>
             <div class="bigLogo">
                 <img src="./images/catpic.jpg" style="width: 50%;margin-left:25%"/>
             </div>
