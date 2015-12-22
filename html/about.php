1ee6209766be09056b41c8967bb35e6bcade7ca8
jcontreras28 <janellec@bendbroadband.com>
2015-12-22 14:00:09 -0800

    content

--- html/about.php
+++ html/about.php
@@ -4,16 +4,18 @@
             <img class="meImage" src="./images/softball.jpg">
         </div>
         <div class="col-md-9">
-            <div class="sectionTitle">
-                Who am I?
+            <div class="resProfTitle">Who am I?</div> 
+            <div class="resText">This page (and me too) are under development.
             </div>
-            <div class="sectionBar">
-            </div>
+
         </div>
     </div><!-- sectionHeading -->
     <div class="row sectionText">
-        <p><strong>Amazing!</strong> First and formost I love to learn.  You give me a challenge and I will take it on with gusto. </p> 
-        <p>I love sports!</p>
+        <div class="limitWidth">
+            <div class="resSecTitle">Dream Job?</div>    
+            <p>First and formost I love to learn.  You give me a challenge and I will take it on with gusto. </p> 
+            <p>I love sports!</p>
+        </div>
     </div><!-- sectionText -->
     <div class="windowwidth">click for window width</div>
 </div><!-- panelWrap -->
